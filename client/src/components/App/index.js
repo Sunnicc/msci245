58597e9606eef264ed565821521624d6a383a0d3
Heesun Choi <h4choi@uwaterloo.ca>
2022-05-16 11:19:38 -0300

    lecture 3-1

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -8,6 +8,35 @@ import {
 import Home from '../Home';
 import PrivateRoute from '../Navigation/PrivateRoute.js';
 
+const title = 'React';
+
+const welcome = {
+  greeting:'Hey',
+  title: 'React',
+}
+
+function getTitle(title){
+  return title;
+}
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  }, 
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+    },
+];
 
 
 class App extends Component {
@@ -33,11 +62,37 @@ class App extends Component {
     return (
 	  <Router>
 	    <div>
-        <PrivateRoute exact path="/" component={Home}/>
+        {/*<h1>
+        1 {welcome.greeting} {welcome.title}
+        </h1>
+
+        <h1>
+          2 {getTitle('React')}
+        </h1>
+
+        <h1>3 Hello world {title}</h1>  */}
+        <ul>
+          {list.map(function(item){
+            return <li>{item.title}</li>;
+          })}
+        </ul>
+        
+        <Search />
+        
+        <PrivateRoute exact path="/" component={Home}/> 
 	    </div>
 	  </Router>
     );
   }
+}
+
+function Search(){
+  return(
+    <div>
+      <label htmlFor='search'>Search :</label>
+      <input id ='search' type='text'/>
+    </div>
+  );
 }
 
 export default App;
